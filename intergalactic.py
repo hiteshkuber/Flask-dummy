b4f8851bd1e2f67cee79e6dc1543f17bc8ddf25f
Hitesh Kuber <hiteshkuber@gmail.com>
2024-11-09 22:33:13 +0530

    template control flow

--- intergalactic.py
+++ intergalactic.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
     my_name = "Hitessssssssssssssssshhhh Kuuberrrrrrrrrrrrrrrrrrrrrrrrr"
-    return render_template("homepage.html", my_name = my_name)
+    reasons = [
+            "Saved the universe twice from intergalactic parasites.",
+            "Engaged in philanthropy, providing potions to alien civilizations in need.",
+            "Enhanced the energy of over 12,432,123 dimming stars, saving countless lives."
+            ]
+    return render_template("homepage.html", my_name = my_name, reasons = reasons)
           
 
 if __name__ == "__main__":
