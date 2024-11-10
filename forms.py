98238b00d8a053b5fdbc1c873990c90e142bc99d
Hitesh Kuber <hiteshkuber@gmail.com>
2024-11-10 21:53:11 +0530

    forms

--- forms.py
+++ forms.py
@@ -4,7 +4,18 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "test"
+    return render_template("index.html")
+
+@app.route("/signup_form")
+def sign_up_form():
+    return render_template("signup.html")
+
+@app.route("/thank_you")
+def thank_you():
+    first  = request.args.get('First')
+    last  = request.args.get('Last')
+
+    return render_template('thankyou.html', first=first, last=last)
 
 if __name__ == "__main__":
     app.run(debug=True)
